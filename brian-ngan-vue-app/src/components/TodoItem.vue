706a8bf85883b9ffcb530be958ff37e886bf84a0
brianngan <brianngan92@gmail.com>
2020-08-09 18:33:28 +0800

    vue todo

---
+++ brian-ngan-vue-app/src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" name="" id="" v-on:change="markComplete">
+      {{todo.title}}</p>
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed =!this.todo.completed;
+    }
+    }
+}
+</script>
+
+<style scoped>
+.todo-item{
+  font-weight: 900;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del{
+  color: red;
+}
+
+ ;
+
+
+</style>
---
+++ brian-ngan-vue-app/src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <h1>TodoList</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    
+</style>
